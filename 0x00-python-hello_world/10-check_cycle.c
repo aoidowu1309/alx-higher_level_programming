791d560e1b63fc7cf2c46852ddfccec3086b583e
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-11-29 03:42:47 -0800

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,23 @@
 #include "lists.h"
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ *
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise, *hare;
-	tortoise = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 	if (!list)
 		return (0);
-	hare = list->next;
-	while (hare && hare->next)
+	while (slow && fast && fast->next)
 	{
-		if (tortoise == hare)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-		hare = hare->next->next;
-		tortoise = tortoise->next;
 	}
 	return (0);
 }
