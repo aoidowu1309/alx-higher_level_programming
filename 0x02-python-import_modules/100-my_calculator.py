d75cf028b3df8b42d18b15c067053ea5110292cd
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-12-02 15:46:19 -0800

    Write a program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    if (len(argv) - 1) != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+ops = ['+', '-', '*', '/']
+argc = len(argv)
+
+if __name__ == '__main__':
+    if (argc - 1) < 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-
-        a = int(argv[1])
-        b = int(argv[3])
-
-        if argv[2] == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-            exit(0)
-        elif argv[2] == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-            exit(0)
-        elif argv[2] == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-            exit(0)
-        elif argv[2] == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-            exit(0)
+    elif argv[2] not in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    else:
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
