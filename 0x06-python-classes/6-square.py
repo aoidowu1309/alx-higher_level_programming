41cca8ae022083a30ae68850668875cd9e980c98
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-12-21 18:06:10 -0800

    Write a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
-
 class Square:
     """Represent a square."""
-
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
@@ -20,10 +18,8 @@ class Square:
 
         @size.setter
         def size(self, value):
-
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-            
             elif value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
@@ -35,7 +31,6 @@ class Square:
 
             @position.setter
             def position(self, value):
-            
                 if (not isinstance(value, tuple) or
                         len(value) != 2 or
                         not all(isinstance(num, int) for num in value) or
@@ -49,14 +44,12 @@ class Square:
 
                 def my_print(self):
                     """Print the square with the # character."""
-                
                 if self.__size == 0:
                         print("")
                         return
 
                     [print("") for i in range(0, self.__position[1])]
-                
-                for i in range(0, self.__size):
+                 for i in range(0, self.__size):
                         [print(" ", end="") for j in range(0, self.__position[0])]
                         [print("#", end="") for k in range(0, self.__size)]
                         print("")
